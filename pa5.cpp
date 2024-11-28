55f4bb5071028c4ff3b9a52f87f29cd3d72f6718
hoang180206 <hoangnguyen29011802@gmail.com>
2024-11-28 23:31:43 +0700

    Update pa

--- pa5.cpp
+++ pa5.cpp
@@ -1,15 +1,1 @@
-#include<stdio.h>
-int main(){
-	int arr[5][5]={{1,2,3,4,5},{6,7,8,9,10}};
-	int max = arr[0][1];
-	for(int i =1;i<10;i++){
-		if(arr[i][i]>max){
-		max=arr[i][i];
-	}
-	}
-		printf("phan tu lon nhat la %d\n",max);
-	return 0;
-	} 
-
-	 
 
